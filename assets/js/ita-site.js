b323dd470802b2444dcae751208dab0cc7547e47
Wreath Vine <wreathvine@gmail.com>
2023-03-16 12:47:51 +0000

    Fixed iamges.

---
+++ assets/js/ita-site.js
@@ -0,0 +1,14 @@
+
+$(function () {
+    $("figure img").click(function () {
+        $("#grayBack").html($(this).prop("outerHTML"));
+        $("#grayBack").fadeIn(200);
+        return false;
+    });
+
+    $("#grayBack").click(function () {
+        $("#grayBack").fadeOut(200);
+        return false;
+    });
+});
+  
